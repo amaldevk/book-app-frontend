1cdd4691f0b3a003eb0d801a41196a54cd2c267d
amaldevk <amalvasudev111@gmail.com>
2024-02-09 03:08:49 +0530

    add_book ok

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBook from './components/NavBook';
+import AddBook from './components/AddBook';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element = {<NavBook/>}/>
+        <Route path='/add' element = {<AddBook/>}/>
       </Routes>
       </BrowserRouter>
     </div>
