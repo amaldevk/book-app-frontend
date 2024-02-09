de59d14a6f21d4fe3525d0d5dbd6acab0329563f
amaldevk <amalvasudev111@gmail.com>
2024-02-09 09:11:35 +0530

    view_book ok

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBook from './components/NavBook';
 import AddBook from './components/AddBook';
+import ViewBook from './components/ViewBook';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element = {<NavBook/>}/>
         <Route path='/add' element = {<AddBook/>}/>
+        <Route path='/view' element = {<ViewBook/>}/>
       </Routes>
       </BrowserRouter>
     </div>
